11f29cd5e5ed4ad325037c09b591d0805a633311
Agostinho Neto <agostinho.neto@petlove.com.br>
2023-02-21 20:38:04 -0300

    created component to login

--- app/javascript/components/login/index.jsx
+++ app/javascript/components/login/index.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
+import { Typography, Box, Button, TextField } from '@mui/material';
+// import {
+//     Container,
+//   } from './style'
+
 
 const Login = () => {
-    return(
-        <div>
-        <label>Login</label>
-        <input></input>
-        <label>Senha</label>
-        <input></input>
-    </div>
-    )
+  return(
+    <Box style={{display: "flex", justifyContent: "center", backgroundColor: "#D4D4D4", width: "100%", height: "100vh"}}>
+        <Box style={{alignSelf: "center",backgroundColor: "white", width: "35vw", height: "60vh", borderRadius: "10px", boxShadow: "0px 10px 25px rgba(0, 0, 0, 0.05)"}}>
+          <Box style={{marginTop: "5%"}}>
+            <Typography style={{ textAlign: "center",fontFamily: 'Courier New', fontStyle: "normal", fontSize: "2rem"}}>Agenda de Contatos V1</Typography>
+          </Box>
+          <Box style={{ display: "flex", flexDirection: "column", marginLeft: "10px", marginTop: "auto", marginBottom: "auto", height: "80%", justifyContent: "center",}}>
+            <Box>
+              <Typography>Informe suas credenciais para acessar a sua agenda</Typography>
+            </Box>
+            <Box>
+              <Typography style={{fontSize: "20px"}} variant='subtitle1'>Email</Typography>
+              <TextField variant='outlined' style={{ width: '92%', marginBottom: '10px' }} />
+            </Box>
+            <Box>
+              <Typography style={{fontSize: "20px"}} variant='subtitle1'>Senha</Typography>
+              <TextField variant='outlined' style={{ width: '92%', marginBottom: '10px' }} />
+            </Box>
+            <Box>
+              <Button variant="contained">Entrar</Button>
+            </Box>
+          </Box>
+        </Box>
+    </Box>
+  )
 }
 
 export default Login
---
+++ app/javascript/components/login/style.js
@@ -0,0 +1,70 @@
+// import styled from 'styled-components'
+// import { Box, Typography, Button } from '@mui/material'
+
+// export const Container = styled(Box)`
+//   height: 100%;
+//   width: 100%;
+//   background-color: #6800cf;
+//   position: absolute;
+//   display: flex;
+//   justify-content: center;
+//   flex-direction: column;
+//   align-items: center;
+// `
+// export const TextContainer = styled(Box)``
+
+// export const FinishedTeachText = styled(Typography)`
+//   color: #ffffff;
+//   font-size: 22px;
+//   text-align: center;
+//   margin-top: 1rem;
+// `
+
+// export const CorrectAnswerText = styled(Typography)`
+//   color: #ffffff;
+//   font-size: 22px;
+//   text-align: center;
+//   margin-top: 0.5rem;
+// `
+
+// export const CoinsContainer = styled(Box)`
+//   background-color: #ffe39b;
+//   box-shadow: 0px 10px 25px rgba(0, 0, 0, 0.05);
+//   border-radius: 100px;
+//   height: 44px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   width: 74px;
+//   margin-top: 1rem;
+//   margin-bottom: 1rem;
+// `
+// export const CoinsImage = styled.img`
+//   width: 26.52px;
+//   height: 30px;
+//   color: #ffc01f;
+// `
+// export const CoinsValue = styled(Typography)`
+//   font-size: 18px;
+//   align-self: left;
+//   color: #edae0b;
+//   margin-left: 0.5rem;
+// `
+
+// export const ReturnToHomeButton = styled(Button)`
+//   background-color: #50c8fb;
+//   text-align: center;
+//   margin-top: 0.5rem;
+//   text-transform: none;
+//   border-radius: 30px;
+//   width: 257px;
+//   height: 50px;
+//   &:hover {
+//     background-color: #3d9cc4;
+//   }
+// `
+
+// export const ReturnToHomeText = styled(Typography)`
+//   color: #ffffff;
+//   font-size: 16px;
+// `
