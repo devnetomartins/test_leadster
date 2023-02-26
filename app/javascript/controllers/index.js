c1a7c80335e7aa0285a19434e7fda93eca050b19
Agostinho Neto <agostinho.neto@petlove.com.br>
2023-02-26 09:37:30 -0300

    registred contacts index controller on stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,7 @@
 import { application } from "./application"
 
 import LoginIndexController from "./login/index_controller"
+import ContactsIndexController from "./contacts/index_controller"
 
 application.register("login-index", LoginIndexController)
+application.register("contacts-index", ContactsIndexController)
