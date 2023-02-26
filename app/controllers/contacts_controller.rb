d387c2e056b5d1323bffe2779222a501954a93c3
Agostinho Neto <agostinho.neto@petlove.com.br>
2023-02-26 09:33:28 -0300

    updated action index in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,18 @@ class ContactsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    contacts = user_contacts.paginate(page: current_page, per_page: params[:per_page] || 20)
+
+    @contacts_serialized = ActiveModelSerializers::SerializableResource.new(contacts, each_serializer: ContactSerializer).to_json
+  end
+
+  private
+
+  def current_page
+    params[:page] || 1
+  end
+
+  def user_contacts
+    @user_contacts ||= current_user.contacts
   end
 end
