72a68401d817593b2a2d3f2aba0dfbe95b26af34
Agostinho Neto <agostinho.neto@petlove.com.br>
2023-02-27 14:12:11 -0300

    added routes to access sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :contacts, only: [:index, :create, :show, :update, :destroy] do
     collection do
@@ -8,6 +10,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(username, ENV['SIDEKIQ_USER'])
+    ActiveSupport::SecurityUtils.secure_compare(password, ENV['SIDEKIQ_PASSWORD'])
+  end
+
+  mount Sidekiq::Web => '/sidekiq'
 
   root :to => redirect('/contacts')
 end
