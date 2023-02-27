aa33b68485e66feca682874a7f7d50150b86f856
Agostinho Neto <agostinho.neto@petlove.com.br>
2023-02-26 22:19:54 -0300

    render editContact modal in listContacts

--- app/javascript/components/contacts/ListContacts/index.jsx
+++ app/javascript/components/contacts/ListContacts/index.jsx
@@ -3,21 +3,25 @@ import { Typography, TextField, Box, InputAdornment, IconButton, Tooltip, Icon} 
 import Header from '../../header'
 import ContactsTable from '../../ContactsTable'
 import NewContact from '../../contacts/NewContact'
+import EditContact from '../../contacts/EditContact'
 // import {
 //     Container,
 //   } from './style'
 
-const ListContacts = ({contacts}) => {
+const ListContacts = ({userEmail, contacts}) => {
   const [openNewContact, setOpenNewContact] = useState(false)
+  const [openEditContact, setOpenEditContact] = useState(false)
+  const [contactId, setContactId] = useState('')
 
   return(
     <Box sx={{ flexGrow: 1 }}>
       <Header />
       <NewContact open={openNewContact} handleClose={() => {setOpenNewContact(false)}}/>
+      <EditContact open={openEditContact} contactId={contactId} handleClose={() => {setOpenEditContact(false)}} />
       <Box style={{display: "flex", flexDirection: "column", alignItems: 'center', backgroundColor: "#D4D4D4", width: "100%", position: 'absolute', height: '100%'}}>
-        <Typography style={{marginTop: '1rem'}}>Olá Agostinho, abaixo você pode cadastrar seus contatos.</Typography>
+        <Typography style={{marginTop: '1rem'}}>{`Olá ${userEmail}, abaixo você pode cadastrar seus contatos.`}</Typography>
         <Box style={{width: "80vw", marginTop: "3rem"}}>
-          <ContactsTable contacts={contacts} handleNewContact={() => {setOpenNewContact(true)}} />
+          <ContactsTable contacts={contacts} setContactId={setContactId} handleEditContact={() => { setOpenEditContact(true) }} handleNewContact={() => {setOpenNewContact(true)}} />
         </Box>
       </Box>
     </Box>
